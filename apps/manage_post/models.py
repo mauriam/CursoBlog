f0fa8da1e17e371013f20ca1ed41bd72f9ad01c4
Mauriam Ortiz <mauriamortiz@gmail.com>
2023-02-22 12:34:09 -0400

    add category article rating

--- apps/manage_post/models.py
+++ apps/manage_post/models.py
@@ -1,3 +1,56 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(blank=False, null=False)
+    slug = models.SlugField(unique=True, max_length=40)
+    featured = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    introduction = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    image = models.ImageField()
+    body = models.TextField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories =models.ManyToManyField(Category)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
+class Rating(models.Model):
+    value =  models.FloatField()
+    description = models.CharField(max_length=255)
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user_id.name
+    
+    class Meta:
+        verbose_name = 'Rating'
+        verbose_name_plural = 'Rating'
