d157e9a891b2a53e70b754a28b6f9441380c87db
Mauriam Ortiz <mauriamortiz@gmail.com>
2023-02-22 12:57:23 -0400

    ADD modelos Admin Django

--- apps/user/models.py
+++ apps/user/models.py
@@ -7,4 +7,12 @@ class User(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     photo = models.ImageField(blank=True,null=True)
+    profession = models.CharField(max_length=50, null=True)
+    about = models.TextField(null=True)
+    birthday = models.DateField(null=True)
+    twitter = models.URLField(max_length=50, null=True)
+    linkedin = models.URLField(max_length=50, null=True)
+    facebook = models.URLField(max_length=50, null=True)
     
+    def __str__(self) -> str:
+        return self.user.username
